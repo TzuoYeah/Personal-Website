190db156445f2d260036e6a340782d5b9c85a3d9
e4225 <tzuoyeah@gmail.com>
2020-03-18 13:34:51 +0800

    3.18

--- JQ_function.js
+++ JQ_function.js
@@ -1,17 +1,3 @@
 $(document).ready(function(){
-    allHide();
-    $("#bt1").click(function(){
-        allHide();
-        $("#con1").show();
-    });
-    $("#bt2").click(function(){
-        allHide();
-        $("#con2").show();
-    });
-    $(".btn").click(function(){
-    })
-});
 
-function allHide(){
-    $(".content").hide();
-}
+});
