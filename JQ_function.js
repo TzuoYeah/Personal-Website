34dd02f1df75e91ccccc12d9ab7cfa936194c46d
e4225 <tzuoyeah@gmail.com>
2020-03-19 17:57:15 +0800

    3.19-2

--- JQ_function.js
+++ JQ_function.js
@@ -1,3 +1,55 @@
 $(document).ready(function(){
-
+    var display="#display";
+    setting(display);
+    $("a").click(function(){
+        let id = $(this).attr("id");
+        seletePage(id);
+    });
 });
+function setting(sel){
+    if(location.hash=="#about"){
+        about(sel);
+    }else if(location.hash=="#works"){
+        works(sel);
+    }else{
+        home(sel);
+    }
+}
+function seletePage(id){
+    if(id =="a_about"){
+        $(display).children().remove();
+        about(display);
+    }else if(id == "a_works"){
+        $(display).children().remove();
+        works(display);
+    }else{
+        $(display).children().remove();
+        home(display);
+    }
+}
+function home(sel){
+    $(sel).append("<img>");
+    $(sel).append("<h2>");
+    $(sel).append("<p>");
+    $("img").attr({src:"180x180.png"});
+    $("h2").text("十元");
+    $("p").text("\"簡介簡介簡介簡介\"");
+}
+
+function about(sel){
+    $(sel).append("<h2>");
+    $(sel).append("<h3>");
+    $(sel).append("<p>");
+    $("h2").text("關於");
+    $("h3").text("經歷");
+    $("p").text("文字文字");
+    
+}
+
+function works(sel){
+    $(sel).append("<h2>");
+    $(sel).append("<p>");
+    $("h2").text("作品");
+    $("p").text("自己做卡片");
+    
+}
